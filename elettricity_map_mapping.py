cfa7c87deeb327767247702a9abc5891355ec8ff
simoneloop <simonelopez@hotmail.it>
2022-04-09 19:47:43 +0200

    parsing all

--- elettricity_map_mapping.py
+++ elettricity_map_mapping.py
@@ -13,7 +13,36 @@ import pandas as pd
 import re
 from selenium.webdriver.chrome.options import Options
 
-dataframe=pd.DataFrame(columns=['type','installed','used','total'])
+
+
+######################## flusso dati
+
+#tutti i watt sono in kw
+#tutti i g sono in kg(tranne carbon_intensity che è gco2/kwh)
+
+
+
+
+
+#per ogni paese un file excel/csv
+#ogni thread avrà accesso ai vari fogli excel a cui ha accesso per gli stati da cercare
+#il singolo thread, apre il foglio excel, ne legge il contenuto, avvia la ricerca per una nuova riga, concatena la nuova ricerca ai vecchi risultati infine sovrascrive il file excel del paese specifico
+
+
+
+
+
+
+
+
+dataframe=pd.DataFrame(columns=['timestamp','carbon_intensity','low emissions','renewable_emissions','total_elettricity','total_emissions'
+    ,'nucleare_installed_capacity','nucleare_emissions','geotermico_installed_capacity','geotermico_emissions'
+    ,'biomasse_installed_capacity','biomasse_emissions','carbone_installed_capacity','carbone_emissions'
+    ,'eolico_installed_capacity','eolico_emissions','fotovoltaico_installed_capacity','fotovoltaico_emissions'
+    ,'idroelettrico_installed_capacity','idroelettrico_emissions','accumuloidro_installed_capacity','accumuloidro_emissions'
+    ,'batteriaaccu_installed_capacity','batteriaaccu_emissions','gas_installed_capacity','gas_emissions'
+    ,'petrolio_installed_capacity','petrolio_emissions','sconosciuto_installed_capacity','sconosciuto_emissions'
+    ,'exchange_export','exchange_import'])
 
 
 chrome_options = Options()
@@ -65,6 +94,7 @@ def get_production_data(data):
             percentage_on_total = float(percentage_on_total.group(0))  # prendo il risultato trovato
             print("                                                            ", percentage_on_total," % di questa fonte sul totale statale")
             production = total_eletricity * (percentage_on_total/100) * flag_deposit
+            production=round(production,2)
 
 
 
@@ -100,6 +130,7 @@ def get_production_data(data):
             percentage_on_total = float(percentage_on_total.group(0))
             print("                                                            ", percentage_on_total, " % di inquinamento di questa fonte sul totale statale")
             emissions= total_emissions * (percentage_on_total / 100)
+            emissions=round(emissions,2)
 
     print("                                                             Capacita' installata Totale", total_eletricity," KW")
 
@@ -112,18 +143,108 @@ def get_production_data(data):
     print("                                                             Produzione", production, " KW")
 
     print("                                                             Emissioni per questa fonte", emissions, " kg CO2 equivalente al minuto")
+    return percentage_on_total,total_eletricity,total_emissions,type,production,emissions
     #TODO parser
 
 def get_exchange_data(data):
-    print()
+    text = data.split('\n')
+    res_import=[] #TODO
+    res_export=[] #TODO
+    exchange_state=None
+    flag_deposit=None
+    if (re.search('esportata', text[0])):
+        exchange_state = text[0].split("in")[1]
+        flag_deposit = -1
+    else:
+        flag_deposit = 1;
+        exchange_state = text[0].split("da")[1]
+
+
+    total_eletricity = text[1].split("/ ")[1].replace(")", "")
+    tmp = re.search("[A-Z]+", total_eletricity)
+    total_eletricity = re.search("[0-9]+['.'][0-9]*|[0-9]+", total_eletricity)
+
+    if (total_eletricity):
+        tmp = tmp.group(0)
+        total_eletricity = float(total_eletricity.group(0))
+
+        if (tmp == 'GW'):
+            total_eletricity = total_eletricity * 1000000
+        elif (tmp == 'MW'):
+            total_eletricity = total_eletricity * 1000
+        elif (tmp == 'W'):
+            total_eletricity = total_eletricity / 1000
+
+        percentage_on_total = re.search("[0-9]+['.'][0-9]*|[0-9]+", text[0])
+
+        if (percentage_on_total):
+            percentage_on_total = float(percentage_on_total.group(0))  # prendo il risultato trovato
+            print("                                                            ", percentage_on_total,
+                  " % di questa fonte sul totale statale")
+            exchange = total_eletricity * (percentage_on_total / 100) * flag_deposit
+            exchange = round(exchange, 2)
+
+    print("stato scambio",exchange_state)
+
+    installed_exchange_capacity = text[4].split("/ ")[1].replace(")", "")
+    tmp = re.search("[A-Z]+", installed_exchange_capacity)
+    installed_exchange_capacity = re.search("[0-9]+['.'][0-9]*|[0-9]+", installed_exchange_capacity)
+
+    if (installed_exchange_capacity):
+        tmp = tmp.group(0)
+        installed_exchange_capacity = float(installed_exchange_capacity.group(0))
+
+        if (tmp == 'GW'):
+            installed_exchange_capacity = installed_exchange_capacity * 1000000
+        elif (tmp == 'MW'):
+            installed_exchange_capacity = installed_exchange_capacity * 1000
+
+        elif (tmp == 'W'):
+            installed_exchange_capacity = installed_exchange_capacity / 1000
+
+    total_exchange_emissions = text[7].split("/ ")[1]
+    total_exchange_emissions = total_exchange_emissions.split(" ")[0]
+
+    tmp = re.search("[a-z]+", total_exchange_emissions)
+    total_exchange_emissions = re.search("[0-9]+['.'][0-9]*|[0-9]+", total_exchange_emissions)
+
+    if (total_exchange_emissions):
+        tmp = tmp.group(0)
+        total_exchange_emissions = float(total_exchange_emissions.group(0))
+        if (tmp == 't'):
+            total_exchange_emissions = total_exchange_emissions * 1000
+        percentage_on_total = re.search("[0-9]+['.'][0-9]*|[0-9]+", text[6])
+        if (percentage_on_total):
+            percentage_on_total = float(percentage_on_total.group(0))
+            print("                                                            ", percentage_on_total,
+                  " % di inquinamento di questa fonte sul totale statale")
+            emissions = total_exchange_emissions * (percentage_on_total / 100)
+            emissions = round(emissions, 2)
+
+    print("                                                             Capacita' installata Totale", total_eletricity,
+          " KW")
+
+    print("                                                             Emissioni  totali", total_exchange_emissions,
+          " kg CO2 equivalente al minuto")
+
+    print("                                                             Capacità installata", installed_exchange_capacity, "KW")
+
+    print("                                                             Scambio", exchange, " KW")
+
+    print("                                                             Emissioni per trasporto", emissions,
+          " kg CO2 equivalente al minuto")
+
+    return res_import,res_export
     #print("exchange \n", data)
 
     #TODO parser
 
 def get_carbon_data(data):
-    print()
-    #for d in data:
-        #print(d.text)
+    carbon_intensity=data[0].text.split("\n")[0].replace("g","")
+    low_emissions=data[1].text.split("\n")[0]
+    renewable_emissions=data[2].text.split("\n")[0]
+    print(carbon_intensity,low_emissions,renewable_emissions)
+
     #TODO parser
 # Press the green button in the gutter to run the script.
 
@@ -146,7 +267,7 @@ if __name__ == '__main__':
 
     #s=Service("chromedriver.exe")
     browser = webdriver.Chrome(service=service)
-    # browser = webdriver.Chrome(service=s,options=chrome_options)
+    #browser = webdriver.Chrome(service=service,options=chrome_options)
 
     browser.get(url_elettricity_map)
     time.sleep(2)
@@ -168,7 +289,7 @@ if __name__ == '__main__':
          if (stato in stati):
             if (len(tag) == 3):
                 zona_nello_stato = tag[len(tag) - 2]
-                print("Stato = ", stato, "zona_nello_stato = ", zona_nello_stato)
+                print("Stato = ", stato, "zona nello stato = ", zona_nello_stato)
             else:
                 print("Stato = ", stato)
 
@@ -178,8 +299,12 @@ if __name__ == '__main__':
             time.sleep(2)
             body=browser.find_elements(By.TAG_NAME,"body")[0]
             time.sleep(2)
-            carbon_data=body.find_elements(By.CLASS_NAME,"country-col")
-            get_carbon_data(carbon_data)
+            try:
+                carbon_data=body.find_elements(By.CLASS_NAME,"country-col")
+            except:
+                pass
+            if(carbon_data):
+                get_carbon_data(carbon_data)
 
             rows = browser.find_elements(By.CLASS_NAME, "row") #prendiamo tutte le righe ognuna delle quali è una fonte energetica o scambio
             time.sleep(2)
@@ -196,20 +321,19 @@ if __name__ == '__main__':
                     try:
                         exchange_popup = body.find_element(By.ID,"countrypanel-exchange-tooltip")
                     except:
-                        print("no")
+                        pass
                 if (production_popup):
                     print()
                     get_production_data(production_popup.text)
 
                 elif (exchange_popup):
                     print()
-                    get_exchange_data(exchange_popup.text)
+                    res_import,res_export=get_exchange_data(exchange_popup.text)
                 print("#############################################################################################################################################")
             back = browser.find_elements(By.CLASS_NAME, "left-panel-back-button")[0]
             back.click()
             zone_list = browser.find_elements(By.CLASS_NAME, "zone-list")[0]
             zones = zone_list.find_elements(By.TAG_NAME, "a")
-
 
 
     browser.close()
