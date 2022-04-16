4ae969eccd1cde19e08ed5237aa53c03cf864766
Glacyal <marinoantonio.1996@gmail.com>
2022-04-17 01:37:36 +0200

    .

--- elettricity_map_mapping.py
+++ elettricity_map_mapping.py
@@ -18,6 +18,7 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--remote-debugging-port=22")
 
 ######################## flusso dati
