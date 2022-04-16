91d72d09db2d18ac77f3d58394336664f1249bd5
Glacyal <marinoantonio.1996@gmail.com>
2022-04-17 01:11:38 +0200

    .

--- elettricity_map_mapping.py
+++ elettricity_map_mapping.py
@@ -18,6 +18,8 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sendbox")
+
 
 ######################## flusso dati
 
