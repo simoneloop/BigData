3525140ad4faa56677c0ffb414f4f53b15ea2c85
Glacyal <marinoantonio.1996@gmail.com>
2022-04-19 17:56:45 +0200

    new19042022

--- comandi.sh
+++ comandi.sh
@@ -3,14 +3,16 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-git clone https://github.com/simoneloop/BigData.git
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 sudo apt install python3-pip
 
 pip install selenium
 pip install pandas
 pip install webdriver-manager
 
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+
+git clone https://github.com/simoneloop/BigData.git
