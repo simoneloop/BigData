4bc9d477bdcdee2bb8b7bc8073aa72abff2d7941
Glacyal <marinoantonio.1996@gmail.com>
2022-04-19 23:14:01 +0200

    .

--- comandi.sh
+++ comandi.sh
@@ -8,9 +8,10 @@ sudo apt install python3-pip
 pip install selenium
 pip install pandas
 pip install webdriver-manager
+pip install openpyxl
+pip install pydrive
 
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
