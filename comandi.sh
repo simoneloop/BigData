4570c5025cf4557b60f2097d5035035680d47aa0
Glacyal <marinoantonio.1996@gmail.com>
2022-04-19 18:21:19 +0200

    .

--- comandi.sh
+++ comandi.sh
@@ -14,5 +14,3 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
-
-git clone https://github.com/simoneloop/BigData.git
