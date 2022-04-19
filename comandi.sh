704af91297745ebbe831aa2e1a47529d96f9235e
Glacyal <marinoantonio.1996@gmail.com>
2022-04-19 17:44:54 +0200

    new19042022

--- comandi.sh
+++ comandi.sh
@@ -8,3 +8,5 @@ git clone https://github.com/simoneloop/BigData.git
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+sudo apt install python3-pip
+
