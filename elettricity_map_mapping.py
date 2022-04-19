290eba0caad6c854d60374d09a97f3404ed1930f
Glacyal <marinoantonio.1996@gmail.com>
2022-04-19 22:26:43 +0200

    .

--- elettricity_map_mapping.py
+++ elettricity_map_mapping.py
@@ -19,7 +19,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 #chrome_options.add_argument("--disable-setuid-sandbox")
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--no-sandbox')
 #chrome_options.add_argument("--remote-debugging-port=22")
 
 
