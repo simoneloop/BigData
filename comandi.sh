9d8d1d9f58e07332f425d3f543fe29da436f0a63
Glacyal <marinoantonio.1996@gmail.com>
2022-05-03 10:39:22 +0200

    caricamento per il nuovo linux

--- comandi.sh
+++ comandi.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+
 sudo apt install python3-pip
 
 pip install selenium
@@ -14,4 +15,9 @@ pip install pydrive
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
+sudo apt-get install -y xvfb
+sudo apt-get -y install xorg xvfb gtk2-engines-pixbuf
+sudo apt-get -y install dbus-x11 xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic xfonts-scalable
+Xvfb -ac :99 -screen 0 1280x1024x16 & export DISPLAY=:99
 
