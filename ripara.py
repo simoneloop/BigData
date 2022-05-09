96735f03f3014913213073be58d9dc6c64473df6
Glacyal <marinoantonio.1996@gmail.com>
2022-05-09 17:05:48 +0200

    tutto

--- ripara.py
+++ ripara.py
@@ -4,70 +4,198 @@ from datetime import datetime
 import os
 import pandas as pd
 
-
-
-
-
 dataframe = pd.DataFrame(
-        columns=['timestamp', 'carbon_intensity', 'low_emissions', 'renewable_emissions', 'total_production',
-                 'total_emissions'
-            , 'nucleare_installed_capacity', 'nucleare_production', 'nucleare_emissions',
-                 'geotermico_installed_capacity', 'geotermico_production', 'geotermico_emissions'
-            , 'biomassa_installed_capacity', 'biomassa_production', 'biomassa_emissions', 'carbone_installed_capacity',
-                 'carbone_production', 'carbone_emissions'
-            , 'eolico_installed_capacity', 'eolico_production', 'eolico_emissions', 'fotovoltaico_installed_capacity',
-                 'fotovoltaico_production', 'fotovoltaico_emissions'
-            , 'idroelettrico_installed_capacity', 'idroelettrico_production', 'idroelettrico_emissions',
-                 'accumuloidro_installed_capacity', 'accumuloidro_production', 'accumuloidro_emissions'
-            , 'batterieaccu_installed_capacity', 'batterieaccu_production', 'batterieaccu_emissions',
-                 'gas_installed_capacity', 'gas_production', 'gas_emissions'
-            , 'petrolio_installed_capacity', 'petrolio_production', 'petrolio_emissions',
-                 'sconosciuto_installed_capacity', 'sconosciuto_production', 'sconosciuto_emissions'
-            , 'exchange_export', 'exchange_import'])
+    columns=['timestamp', 'carbon_intensity', 'low_emissions', 'renewable_emissions', 'total_production',
+             'total_emissions'
+        , 'nucleare_installed_capacity', 'nucleare_production', 'nucleare_emissions',
+             'geotermico_installed_capacity', 'geotermico_production', 'geotermico_emissions'
+        , 'biomassa_installed_capacity', 'biomassa_production', 'biomassa_emissions', 'carbone_installed_capacity',
+             'carbone_production', 'carbone_emissions'
+        , 'eolico_installed_capacity', 'eolico_production', 'eolico_emissions', 'fotovoltaico_installed_capacity',
+             'fotovoltaico_production', 'fotovoltaico_emissions'
+        , 'idroelettrico_installed_capacity', 'idroelettrico_production', 'idroelettrico_emissions',
+             'accumuloidro_installed_capacity', 'accumuloidro_production', 'accumuloidro_emissions'
+        , 'batterieaccu_installed_capacity', 'batterieaccu_production', 'batterieaccu_emissions',
+             'gas_installed_capacity', 'gas_production', 'gas_emissions'
+        , 'petrolio_installed_capacity', 'petrolio_production', 'petrolio_emissions',
+             'sconosciuto_installed_capacity', 'sconosciuto_production', 'sconosciuto_emissions'
+        , 'exchange_export', 'exchange_import'])
 
-stati = ['El Hierro (Spagna)', 'Svezia', 'Francia','Danimarca orientale (Danimarca)', 'Romania',
-             'Spagna', 'Belgio', 'Lettonia', 'Portogallo', 'Gran Canaria (Spagna)',
-             'Danimarca occidentale (Danimarca)', 'Ungheria', 'Formentera (Spagna)', 'Lussemburgo', 'Finlandia',
-             'Austria', 'Slovenia', 'Maiorca (Spagna)', 'Paesi Bassi', 'Lituania',
-             'Slovacchia', 'Croazia', 'Ibiza (Spagna)', 'Polonia', 'Irlanda',
-             'Sicilia (Italia)', 'Bulgaria', 'Tenerife (Spagna)', 'Germania', 'Centronord (Italia)',
-             'Cipro', 'Minorca (Spagna)', 'Settentrione (Italia)', 'Estonia', 'Fuerteventura Lanzarote (Spagna)',
-             'La Palma (Spagna)', 'Meridione (Italia)', 'La Gomera (Spagna)', 'Centrosud (Italia)', 'Sardegna (Italia)']
+stati = ['El Hierro (Spagna)', 'Svezia', 'Francia', 'Danimarca orientale (Danimarca)', 'Romania',
+         'Spagna', 'Belgio', 'Lettonia', 'Portogallo', 'Gran Canaria (Spagna)',
+         'Danimarca occidentale (Danimarca)', 'Ungheria', 'Formentera (Spagna)', 'Lussemburgo', 'Finlandia',
+         'Austria', 'Slovenia', 'Maiorca (Spagna)', 'Paesi Bassi', 'Lituania',
+         'Slovacchia', 'Croazia', 'Ibiza (Spagna)', 'Polonia', 'Irlanda',
+         'Sicilia (Italia)', 'Bulgaria', 'Tenerife (Spagna)', 'Germania', 'Centronord (Italia)',
+         'Cipro', 'Minorca (Spagna)', 'Settentrione (Italia)', 'Estonia', 'Fuerteventura Lanzarote (Spagna)',
+         'La Palma (Spagna)', 'Meridione (Italia)', 'La Gomera (Spagna)', 'Centrosud (Italia)', 'Sardegna (Italia)']
 
 STATES_DIR = "statesRiparati"
 
-if __name__ == '__main__':
-    #statii = ['Germania']
+
+def buchi(lista) :
+    x = 0
+    c = 0
+    l = []
+    while (x < len(lista)) :
+        cont = 0
+        for i in lista[x] :
+            if (i == None) :
+                cont += 1
+        if (cont >= 40) :
+            c += 1
+        else :
+            cont = 0
+        if (c > 0 and cont == 0) :
+            l.append([c, x - c])
+            c = 0
+        x = x + 1
+
+    print(l)
+    return l
+
+
+def riparaBuco(l, nuovofile) :
+    file = []
+    x = 0
+    for i in l :
+        buco = i[1]
+        while (x < buco) :
+            file.append(nuovofile[x])
+            x += 1
+        if (i[0] == 1) :
+            tmp = []
+            tmp.append(nuovofile[x][0])
+            if (x <= 1) :
+                y = 1
+            else :
+                y = -1
+            j = 1
+            while (j < len(nuovofile[x + y])) :
+                tmp.append(nuovofile[x + y][j])
+                j += 1
+
+            file.append(tmp)
+            x += 1
+
+    while (x < len(nuovofile)) :
+        file.append(nuovofile[x])
+        x += 1
+    return file
+
+
+def riparaBuchigrandi(l, nuovofile) :
+    file = []
+    x = 0
+    for i in l :
+        buco = i[1]
+        while (x < buco) :
+            file.append(nuovofile[x])
+            x += 1
+
+        if (i[0] >= 15) :
+            c = 0
+            while (c < i[0]) :
+                c += 1
+                tmp = []
+                tmp.append(nuovofile[x][0])
+                if (x <= 144 + 1) :
+                    y = 144
+                else :
+                    y = -144
+                j = 1
+                while (j < len(nuovofile[x + y])) :
+                    tmp.append(nuovofile[x + y][j])
+                    j += 1
+
+                file.append(tmp)
+                x += 1
+
+    while (x < len(nuovofile)) :
+        file.append(nuovofile[x])
+        x += 1
+    return file
+
+def riparaBuchim(l, nuovofile) :
+    file = []
+    x = 0
+    for i in l :
+        buco = i[1]
+        while (x < buco) :
+            file.append(nuovofile[x])
+            x += 1
+        if (i[0] > 1 and i[0]<15) :
+            tmp = []
+            tmp.append(nuovofile[x][0])
+            if (x <= 1) :
+                y = 1
+            else :
+                y = -1
+            j = 1
+            while (j < len(nuovofile[x + y])) :
+                tmp.append(nuovofile[x + y][j])
+                j += 1
+            #print("Primo ", tmp)
+            file.append(tmp)
+            x += 1
+            c=2
+            while (c < i[0]) :
+                c+=1
+                file.append(nuovofile[x])
+                #print("xxxxx ", nuovofile[x])
+                x += 1
+
+            tmp = []
+            tmp.append(nuovofile[x][0])
+            if (x >= len(nuovofile)) :
+                y = -1
+            else :
+                y = +1
+            j = 1
+            while (j < len(nuovofile[x + y])) :
+                tmp.append(nuovofile[x + y][j])
+                j += 1
+            #print("ULTIMO ",tmp)
+            file.append(tmp)
+            x += 1
+
+    while (x < len(nuovofile)) :
+        file.append(nuovofile[x])
+        x += 1
+    return file
+
+if __name__ == '__main__' :
+    statii = ['Germania']
     start = time.time()
     for s in stati:
-        try:
+        try :
             print(s)
-            file_excel = openpyxl.load_workbook('states/'+s+'.xlsx')
+            file_excel = openpyxl.load_workbook('states/' + s + '.xlsx')
             sheet_obj = file_excel.active
-            numr=sheet_obj.max_row+1
-            numc=sheet_obj.max_column+1
-
+            numr = sheet_obj.max_row + 1
+            numc = sheet_obj.max_column + 1
+            print("numero di rig =", numr - 1)
+            print("numero di col =", numc - 1)
 
             # ripara timestamp non alliniati a 00 10 20 30 40 50
-            rnew=[]
+            rnew = []
 
             c = []
-            for j in range(1, numc):
-                        c.append(sheet_obj.cell(row=1, column=j).value)
+            for j in range(1, numc) :
+                c.append(sheet_obj.cell(row=1, column=j).value)
             rnew.append(c)
 
+            for i in range(2, numr) :
+                c = []
+                for j in range(1, numc) :
+                    tmp = sheet_obj.cell(row=i, column=j).value
 
-            for i in range(2,numr):
-                c=[]
-                for j in range(1, numc):
-                    tmp=sheet_obj.cell(row=i, column=j).value
-
-                    if j != 1:
+                    if j != 1 :
                         c.append(tmp)
-                    else:
-                        if(tmp[4] != "0"):
-                            c.append(tmp[0:4] + "0" + tmp[5:])
-                        else:
+                    else :
+                        if (tmp[4] != "0") :
+                            c.append(tmp[0 :4] + "0" + tmp[5 :])
+                        else :
                             c.append(tmp)
                 rnew.append(c)
 
@@ -75,52 +203,102 @@ if __name__ == '__main__':
             #######################################################################################################################################################################
 
             # riparare i buchi causati dal crash del sistema
-            starttime=timestamp=int(round(datetime.strptime(rnew[1][0],'%H:%M %d-%m-%Y').timestamp()))
+            starttime = timestamp = int(round(datetime.strptime(rnew[1][0], '%H:%M %d-%m-%Y').timestamp()))
 
-            nuovofile=[]
+            nuovofile = []
             nuovofile.append(rnew[0])
 
-            for i in range(1,numr-1):
-                timestamp=int(round(datetime.strptime(rnew[i][0],'%H:%M %d-%m-%Y').timestamp()))
+            for i in range(1, numr - 1) :
+                timestamp = int(round(datetime.strptime(rnew[i][0], '%H:%M %d-%m-%Y').timestamp()))
                 starttime += 600
 
-                if(timestamp==starttime):
+                if (timestamp == starttime) :
                     nuovofile.append(rnew[i])
 
-                else:
-                    while(timestamp>starttime):
+                else :
+                    while (timestamp > starttime) :
                         timestampnew = datetime.fromtimestamp(starttime).strftime('%H:%M %d-%m-%Y')
-                        nuovofile.append([timestampnew, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None])
+                        nuovofile.append(
+                            [timestampnew, None, None, None, None, None, None, None, None, None, None, None, None, None,
+                             None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+                             None, None, None, None, None, None, None, None, None, None, None, None, None, None, None])
 
                         starttime += 600
                     nuovofile.append(rnew[i])
             #######################################################################################################################################################################
+            print(len(nuovofile))
 
-            #qui sotto gestisco il salvataggio del nuovo file
-            #s=s+"NEW"
+            l = buchi(nuovofile)
+            nuovofile = riparaBuco(l, nuovofile)
+            print("Eliminati buchi pari a 1")
+
+            l = buchi(nuovofile)
+            rimedia=False
+            for i in l :
+                if (i[0] > 15) :
+                    rimedia = True
+                    break
+            while(rimedia):
+                nuovofile = riparaBuchigrandi(l, nuovofile)
+                print("Eliminati buchi >= 15")
+                l = buchi(nuovofile)
+                rimedia=False
+                for i in l:
+                    if(i[0]>15):
+                        rimedia=True
+                        break
+
+
+            rimedia = False
+            for i in l :
+                if (i[0] > 1 and i[0] < 15) :
+                    rimedia = True
+                    break
+            while (rimedia):
+                nuovofile = riparaBuchim(l, nuovofile)
+                print("Eliminati buchi tra 2 e 14 inclusi")
+                l = buchi(nuovofile)
+                rimedia = False
+                for i in l :
+                    if (i[0] > 1 and i[0]<15) :
+                        rimedia = True
+                        break
+            if(l!=[]):
+                nuovofile = riparaBuco(l, nuovofile)
+                print("Eliminati buchi pari a 1")
+                l = buchi(nuovofile)
+
+            print(len(nuovofile))
+            #######################################################################################################################################################################
+            # qui sotto gestisco il salvataggio del nuovo file
+            #s = s + "NEW"
 
             path = os.path.join(STATES_DIR, s + ".xlsx")
 
-            dfaaa = pd.DataFrame(columns=dataframe.columns)#44 colonne
-            for i in range(1, len(nuovofile)):
+            dfaaa = pd.DataFrame(columns=dataframe.columns)  # 44 colonne
+
+            for i in range(1, len(nuovofile)) :
                 j = 0
                 tmp = {}
-                for df in dataframe:
+                for df in dataframe :
                     tmp[df] = nuovofile[i][j]
                     j += 1
                 dfbbb = pd.concat([dataframe.copy(), pd.DataFrame(tmp, index=[0])], ignore_index=True)
-                dfaaa = pd.concat([dfaaa,  dfbbb])
+                dfaaa = pd.concat([dfaaa, dfbbb])
 
-            try:
-                if (len(s) <= 30):
+            try :
+                if (len(s) <= 30) :
+
                     dfaaa.to_excel(path, sheet_name=s, index=False)
-                else:
+                else :
                     s1 = s.split(" ")[0]
                     dfaaa.to_excel(path, sheet_name=s1, index=False)
 
-            except Exception as e:
+            except Exception as e :
                 pass
-        except Exception as e:
+
+        except Exception as e :
             print(e)
+
     end = time.time()
     print(end - start)
