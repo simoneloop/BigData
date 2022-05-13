b7f7c46a47496a8210bd89a74df2acbc777846ed
Glacyal <marinoantonio.1996@gmail.com>
2022-05-13 20:03:11 +0200

    ...

--- spark_core.py
+++ spark_core.py
@@ -8,25 +8,45 @@ import os
 
 
 if __name__ == '__main__':
+
     from pyspark.sql import SparkSession
 
-    spark = SparkSession.builder.getOrCreate()
     print("INIZIO")
 
     spark = SparkSession.builder.appName('core').getOrCreate()
-
+    '''
 
     path = "./states"
 
     print(os.listdir(path))
+
     for f in os.listdir(path):
         print(f)
         xcel = pd.read_excel(path + "/" + f)
         f=f.split(".")[0]
-
+        xcel["stato"]=f
 
         xcel.to_csv("./statesCSV/"+ f +".csv", index=False)
-        #rdd = spark.sparkContext.textFile(path + "/Austria.csv")
+    
+    df=0
+    count=0
+    path1="./statesCSV"
+    print(os.listdir(path1))
+    for f in os.listdir(path1):
+        if(count == 0):
+            df=pd.read_csv(path1 + "/" + f)
+            count = 1
+        else:
+            df=pd.concat([df,pd.read_csv(path1 + "/" + f)])
+    df.to_csv("./statesCSV/" + "totalstates" + ".csv", index=False)
+    '''
+    rdd0 = spark.sparkContext.textFile("C:/workSpacepy/BigData/statesCSV/totalstates.csv")
+
+    #for r in rdd3.collect():
+    #    print(r)
+    #rdd0.max(carbon"_intensity)
+
+    #rdd = spark.sparkContext.textFile(path + "/Austria.csv")
 
     #rdd = spark.sparkContext.textFile("./statesCSV/" + "Austria.csv")
     #rdd=rdd.collect()
